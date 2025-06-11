126b56938d3d4198a7975dc9b18e84fec184bfb0
José Ignacio Ramón <joseignacioramon24@gmail.com>
2025-06-11 15:43:07 -0300

    Corrección de un pequeño error de etiquetas que impedia ejecutar el proyecto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 const Home = () => {
   return (
-
     <div className="container my-5 text-center">
       <h1 className="mb-4">Bienvenido a Rolling Palace</h1>
-      <p className="lead">Explorá nuestras habitaciones y hacé tu reserva fácilmente.</p>
+      <p className="lead">
+        Explorá nuestras habitaciones y hacé tu reserva fácilmente.
+      </p>
+    </div>
   );
 };
 
