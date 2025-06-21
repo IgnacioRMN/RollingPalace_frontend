ea6680db816d6bc96258e396c20ac36b9550aaee
José Ignacio Ramón <joseignacioramon24@gmail.com>
2025-06-21 17:14:44 -0300

    ajuste de estilos del navbar y footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import Home from "./pages/Home";
 import "./styles/App.css";
 import Navbar from "./componets/navbar";
 import Footer from "./componets/footer";
-import "./styles/Navbar.css"
+import "./styles/Navbar.css";
 
 function App() {
   return (
     <BrowserRouter>
-     <Navbar />
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-      </Routes>
-     <Footer/>
+      <Navbar />
+      <main style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+      <Footer />
     </BrowserRouter>
   );
 }
